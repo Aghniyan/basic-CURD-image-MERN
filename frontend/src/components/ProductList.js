16b2115553a73f354fba213cbf162604925eee46
Aghniya Ni'amillah Nurhilman <aghniyan@gmail.com>
2022-11-27 14:00:23 +0700

    frist commit CRUD with Image

---
+++ frontend/src/components/ProductList.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {Link} from "react-router-dom";
+
+function ProductList() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+  const getProducts = async () => {
+    const response = await axios.get("http://localhost:5000/products");
+    setProducts(response.data);
+  };
+
+  const deleteProduct = async(productId) => {
+    try {
+      await axios.delete(`http://localhost:5000/products/${productId}`);
+      getProducts();
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+  return (
+    <div className="container mt-5">
+      <Link to="/add" className="button is-success">Add New</Link>
+      <div className="columns is-multiple">
+        {products.map((product) => (
+          <div key={product.id} className="column is-one-quarter">
+            <div className="card">
+              <div className="card-image">
+                <figure className="image is-4by3">
+                  <img
+                    src={product.url}
+                    alt="image"
+                  />
+                </figure>
+              </div>
+              <div className="card-content">
+                <div className="media">
+                  <div className="media-content">
+                    <p className="title is-4">{product.name}</p>
+                  </div>
+                </div>
+              </div>
+              <footer className="card-footer">
+                <Link to={`/edit/${product.id}`} className="card-footer-item">Edit</Link>
+                <a className="card-footer-item" onClick={()=>deleteProduct(product.id)}>Delete</a>
+              </footer>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ProductList;
